9d6768cc5abc8fef66c3a8f7cf92fe26aa67dfb8
David Samy <davidasamy@gmail.com>
2022-06-21 13:13:30 -0700

    comments

--- sleap_io/model/instance.py
+++ sleap_io/model/instance.py
@@ -504,17 +504,17 @@ class Instance:
             self._points = new_array
             self._nodes = self.skeleton.nodes
 
-    @property
-    def points_array(self) -> np.ndarray:
-        """Return array of x and y coordinates for visible points.
+    # @property
+    # def points_array(self) -> np.ndarray:
+    #     """Return array of x and y coordinates for visible points.
 
-        Row in array corresponds to order of points in skeleton. Invisible points will
-        be denoted by NaNs.
+    #     Row in array corresponds to order of points in skeleton. Invisible points will
+    #     be denoted by NaNs.
 
-        Returns:
-            A numpy array of of shape `(n_nodes, 2)` point coordinates.
-        """
-        return self.get_points_array(invisible_as_nan=True)
+    #     Returns:
+    #         A numpy array of of shape `(n_nodes, 2)` point coordinates.
+    #     """
+    #     return self.get_points_array(invisible_as_nan=True)
 
     @property
     def centroid(self) -> np.ndarray:
@@ -644,17 +644,17 @@ class PredictedInstance(Instance):
         if self.from_predicted is not None:
             raise ValueError("PredictedInstance should not have from_predicted.")
 
-    @property
-    def points_and_scores_array(self) -> np.ndarray:
-        """Return the instance points and scores as an array.
+    # @property
+    # def points_and_scores_array(self) -> np.ndarray:
+    #     """Return the instance points and scores as an array.
 
-        This will be a `(n_nodes, 3)` array of `(x, y, score)` for each predicted point.
+    #     This will be a `(n_nodes, 3)` array of `(x, y, score)` for each predicted point.
 
-        Rows in the array correspond to the order of points in skeleton. Invisible
-        points will be represented as NaNs.
-        """
-        pts = self.get_points_array(full=True, copy=True, invisible_as_nan=True)
-        return pts[:, (0, 1, 4)]  # (x, y, score)
+    #     Rows in the array correspond to the order of points in skeleton. Invisible
+    #     points will be represented as NaNs.
+    #     """
+    #     pts = self.get_points_array(full=True, copy=True, invisible_as_nan=True)
+    #     return pts[:, (0, 1, 4)]  # (x, y, score)
 
     @property
     def scores(self) -> np.ndarray:
