86acd7ced35e43b2be3fc4a94ea64d373d739e8f
David Samy <davidasamy@gmail.com>
2022-06-22 15:50:51 -0700

    Test skeleton.py

--- tests/model/test_skeleton.py
+++ tests/model/test_skeleton.py
@@ -1,6 +1,14 @@
 from sleap_io.model.skeleton import Node, Edge, Skeleton
 
 
-def test_node():
-    assert Node("head").name == "head"
-    assert Node("left eye") != Node("right eye")
+def test_skeleton_node_edge():
+    skeleton = Skeleton.from_names(
+        nodes=["head", "thorax", "abdomen"],
+        edges=[("head", "thorax"), ("thorax", "abdomen")],
+    )
+    assert skeleton.name is not None
+    assert skeleton.nodes == [Node("head"), Node("thorax"), Node("abdomen")]
+    assert skeleton.edges == [
+        Edge(source=Node("head"), destination=Node("thorax")),
+        Edge(source=Node("thorax"), destination=Node("abdomen")),
+    ]
