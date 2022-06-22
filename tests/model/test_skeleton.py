cb698555ee59d72edf46433333708388834c2016
David Samy <davidasamy@gmail.com>
2022-06-22 16:12:56 -0700

    test_skeleton.py comments

--- tests/model/test_skeleton.py
+++ tests/model/test_skeleton.py
@@ -2,11 +2,15 @@ from sleap_io.model.skeleton import Node, Edge, Skeleton
 
 
 def test_skeleton_node_edge():
+    # creates a test skeleton with nodes & edges
     skeleton = Skeleton.from_names(
         nodes=["head", "thorax", "abdomen"],
         edges=[("head", "thorax"), ("thorax", "abdomen")],
     )
-    assert skeleton.name is not None
+    assert (
+        skeleton.name
+        is not None  # name should be "Skeleton-n" nth skeleton to be created
+    )
     assert skeleton.nodes == [Node("head"), Node("thorax"), Node("abdomen")]
     assert skeleton.edges == [
         Edge(source=Node("head"), destination=Node("thorax")),
