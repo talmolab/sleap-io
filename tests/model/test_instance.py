7a679e5267cecd4aee823918b44bda4c01c20287
David Samy <davidasamy@gmail.com>
2022-06-24 12:44:11 -0700

    Testing instance

--- tests/model/test_instance.py
+++ tests/model/test_instance.py
@@ -8,8 +8,24 @@ from sleap_io.model.instance import (
     PredictedInstance,
 )
 from sleap_io.model.skeleton import Skeleton, Node
-from sleap_io.model.video import DummyVideo
 import numpy as np
+from attrs import define
+
+
+@define
+class DummyVideo:
+    """Fake video backend,returns frames with all zeros.
+
+    This can be useful when you want to look at labels for a dataset but don't
+    have access to the real video.
+    """
+
+    filename: str = ""
+    height: int = 2000
+    width: int = 2000
+    frames: int = 10000
+    channels: int = 1
+    dummy: bool = True
 
 
 def test_classes():
