a35874739ddbc83e8a14c9ca8b8d6570bd9e313c
David Samy <davidasamy@gmail.com>
2022-06-24 12:39:03 -0700

    Test video.py trivial

--- tests/model/test_video.py
+++ tests/model/test_video.py
@@ -2,11 +2,12 @@ from multiprocessing import dummy
 from statistics import median_grouped
 from tokenize import single_quoted
 from sleap_io.model.video import (
-    HDF5Video,
-    NumpyVideo,
-    MediaVideo,
-    ImgStoreVideo,
-    SingleImageVideo,
-    DummyVideo,
     Video,
 )
+
+
+def test_video_class():
+    test_video = Video(filename="123.mp4", shape=(1, 2, 3, 4), backend=None)
+    assert test_video.filename == "123.mp4"
+    assert test_video.shape == (1, 2, 3, 4)
+    assert Video.fixup_path("123.mp4") == "123.mp4"
