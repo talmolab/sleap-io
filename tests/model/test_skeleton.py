a9f5d68c7b4b1d01c4c2c28ff54d5da2bdb9baea
David <davidasamy@gmail.com>
2022-07-01 14:36:38 -0700

    Pass linter + comment fix

--- tests/model/test_skeleton.py
+++ tests/model/test_skeleton.py
@@ -2,7 +2,7 @@ from sleap_io import Node, Edge, Skeleton
 
 
 def test_skeleton_node_edge():
-    # Creates a test skeleton with nodes & edges.
+    # Creates a test skeleton with nodes and edges.
     skeleton = Skeleton(
         nodes=[Node("head"), Node("thorax"), Node("abdomen")],
         edges=[
@@ -10,6 +10,7 @@ def test_skeleton_node_edge():
             Edge(source=Node("thorax"), destination=Node("abdomen")),
         ],
     )
+    # Asserts that the skeleton has the matching Node and Edge objects.
     assert skeleton.nodes == [Node("head"), Node("thorax"), Node("abdomen")]
     assert skeleton.edges == [
         Edge(source=Node("head"), destination=Node("thorax")),
