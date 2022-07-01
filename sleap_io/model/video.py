b07d3fbfdb6fa275c3dd2f1505c438a0366c5efc
David <davidasamy@gmail.com>
2022-07-01 14:56:12 -0700

    Merging Issues

--- sleap_io/model/video.py
+++ sleap_io/model/video.py
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-"""Data model for Videos.
-
-Videos are SLEAP data structures that store information regarding a video and its
-components used in SLEAP.
-"""
-
-from __future__ import annotations
-from attr import attrs
-from attrs import define, field, Factory
-from typing import Any, List, Optional, Tuple, Union, Dict
-import h5py as h5
-import numpy as np
-import os
-
-
-@define
-class Video:
-    """Video class used by sleap to represent videos and data associated with them.
-
-    This class is used to store information regarding a video and its components.
-    It is used to store the video's filename, shape, and the video's skeleton.
-
-    Args:
-        filename: The filename of the video.
-        shape: The shape of the video.
-        backend: A backend is an object that implements the following basic
-            required methods and properties
-=======
 """Data model for videos.
 
 The `Video` class is a SLEAP data structure that stores information regarding
@@ -50,14 +21,9 @@ class Video:
         shape: The shape of the video. In the format: (frames, height, width, channels).
         backend: An object that implements the basic methods for reading and
             manipulating frames of a specific video type.
->>>>>>> 1adc7affdfb67d755252e1d54de1ecab3ac4f472
 
     """
 
     filename: str
-<<<<<<< HEAD
-    shape: Tuple[int, int, int, int] = field(default=None)
-=======
     shape: Tuple[int, int, int, int]
->>>>>>> 1adc7affdfb67d755252e1d54de1ecab3ac4f472
     backend: Any = None
---
+++ tests/io/test_dataset.py
@@ -0,0 +1,12 @@
+from sleap_io import (
+    from_pointsarray,
+    from_instance,
+    read_hdf5,
+    read_videos,
+    read_skeleton,
+    read_tracks,
+    read_instances,
+    read_metadata,
+    read_points,
+    read_pred_points,
+)
