8ddbe3bb2be0afcb35c43372fe5ed6b21bcb148e
Talmo Pereira <talmo@salk.edu>
2022-07-18 09:49:11 -0700

    Labeled frame

--- sleap_io/model/labeled_frame.py
+++ sleap_io/model/labeled_frame.py
@@ -1,4 +1,4 @@
-"""Data structured for data contained within a single video frame.
+"""Data structures for data contained within a single video frame.
 
 The `LabeledFrame` class is a data structure that contains `Instance`s and
 `PredictedInstance`s that are associated with a single frame within a video.
@@ -6,9 +6,9 @@ The `LabeledFrame` class is a data structure that contains `Instance`s and
 
 from __future__ import annotations
 from sleap_io import Instance, PredictedInstance, Video
-from attrs import define
-import attr
+from attrs import define, field
 from typing import Union
+import numpy as np
 
 
 @define(auto_attribs=True)
@@ -21,30 +21,35 @@ class LabeledFrame:
         instances: List of `Instance` objects associated with this `LabeledFrame`.
     """
 
-    def _set_instance_frame(
-        self, attribute, new_instances: list[Instance]
-    ) -> list[Instance]:
-        """Set the list of `Instance` objects associated with this `LabeledFrame`.
-
-        Updates the `frame` attribute on each `Instance` to the
-        :class:`LabeledFrame` which will contain the `Instance`.
-        The list of `Instance` objects replaces `Instance` objects that were previously
-        associated with `LabeledFrame`.
-
-        Args:
-            instances: A list of `Instance` objects associated with this `LabeledFrame`.
-
-        Returns:
-            None
-        """
-        # Make sure to set the frame for each instance to this LabeledFrame
-        for instance in new_instances:
-            instance.frame = self
-
-        return new_instances
-
     video: Video
     frame_idx: int
-    instances: list[Union[Instance, PredictedInstance]] = attr.ib(
-        factory=list, on_setattr=_set_instance_frame
-    )
+    instances: list[Union[Instance, PredictedInstance]] = field(factory=list)
+
+    def __len__(self) -> int:
+        """Return the number of instances in the frame."""
+        return len(self.instances)
+
+    @property
+    def user_instances(self) -> list[Instance]:
+        """Frame instances that are user-labeled (`Instance` objects)."""
+        return [inst for inst in self.instances if type(inst) == Instance]
+
+    @property
+    def predicted_instances(self) -> list[Instance]:
+        """Frame instances that are user-labeled (`PredictedInstance` objects)."""
+        return [inst for inst in self.instances if type(inst) == PredictedInstance]
+
+    def numpy(self) -> np.ndarray:
+        """Return all instances in the frame as a numpy array.
+
+        Returns:
+            Points as a numpy array of shape `(n_instances, n_nodes, 2)`.
+
+            Note that the order of the instances is arbitrary.
+        """
+        n_instances = len(self.instances)
+        n_nodes = len(self.instances[0]) if n_instances > 0 else 0
+        pts = np.full((n_instances, n_nodes, 2), np.nan)
+        for i, inst in enumerate(self.instances):
+            pts[i] = inst.numpy()
+        return pts
--- tests/model/test_labeled_frame.py
+++ tests/model/test_labeled_frame.py
@@ -1,9 +1,21 @@
-# def test_labeled_frame(dummy_labeled_frame):
-#     lf = dummy_labeled_frame
+from numpy.testing import assert_equal
+from sleap_io import Video, Skeleton, Instance, PredictedInstance
+from sleap_io.model.labeled_frame import LabeledFrame
 
-#     # LabeledFrame
-#     assert lf.video == dummy_video
-#     assert lf.instances == [instance1]
-#     assert lf.instances
-#     lf.instances = [instance1]
-#     assert lf.instances == [instance1]
+
+def test_labeled_frame():
+    lf = LabeledFrame(
+        video=Video(filename="test", shape=(1, 1, 1, 1)),
+        frame_idx=0,
+        instances=[
+            Instance([[0, 1], [2, 3]], skeleton=Skeleton(["A", "B"])),
+            PredictedInstance([[4, 5], [6, 7]], skeleton=Skeleton(["A", "B"])),
+        ],
+    )
+    assert_equal(lf.numpy(), [[[0, 1], [2, 3]], [[4, 5], [6, 7]]])
+
+    assert len(lf) == 2
+    assert len(lf.user_instances) == 1
+    assert type(lf.user_instances[0]) == Instance
+    assert len(lf.predicted_instances) == 1
+    assert type(lf.predicted_instances[0]) == PredictedInstance
