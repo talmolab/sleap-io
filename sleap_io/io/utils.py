c7a5cef7d6a4e74246312e50d0122c22d9fcba82
Talmo Pereira <talmo@salk.edu>
2022-07-19 11:24:29 -0700

    Add utility to read HDF5 attributes

--- sleap_io/io/utils.py
+++ sleap_io/io/utils.py
@@ -1,10 +1,14 @@
 """Miscellaneous utilities for working with different I/O formats."""
 
+from __future__ import annotations
 import h5py
 import numpy as np
+from typing import Any, Union, Optional
 
 
-def read_hdf5(filename: str, dataset: str = "/") -> dict[str, np.ndarray]:
+def read_hdf5(
+    filename: str, dataset: str = "/"
+) -> Union[np.ndarray, dict[str, np.ndarray]]:
     """Read data from an HDF5 file.
 
     Args:
@@ -29,3 +33,26 @@ def read_hdf5(filename: str, dataset: str = "/") -> dict[str, np.ndarray]:
         elif type(f[dataset]) == h5py.Dataset:
             data = f[dataset][()]
     return data
+
+
+def read_hdf5_attrs(
+    filename, dataset: str = "/", attribute: Optional[str] = None
+) -> Union[Any, dict[str, Any]]:
+    """Read attributes from an HDF5 dataset.
+
+    Args:
+        filename: Path to an HDF5 file.
+        dataset: Path to a dataset or group from which attributes will be read.
+        attribute: If specified, the attribute name to read. If `None` (the default),
+            all attributes for the dataset will be returned.
+
+    Returns:
+        The attributes in a dictionary, or the attribute field is `attribute` was
+        provided.
+    """
+    with h5py.File(filename, "r") as f:
+        ds = f[dataset]
+        if attribute is None:
+            return dict(ds.attrs)
+        else:
+            return ds.attrs[attribute]
--- tests/io/test_slp.py
+++ tests/io/test_slp.py
@@ -9,76 +9,23 @@ from sleap_io import (
     Point,
     PredictedPoint,
     PredictedInstance,
+    Labels,
 )
 from sleap_io.io.slp import (
-    instance_from_numpy,
-    predicted_from_instance,
     read_hdf5,
     read_videos,
-    read_skeleton,
+    read_skeletons,
     read_tracks,
     read_instances,
     read_metadata,
     read_points,
     read_pred_points,
+    read_instances,
     read_labels,
 )
 import numpy as np
 
 
-def test_read(slp_file1, slp_file2):
-
-    skeleton = Skeleton(
-        nodes=[Node("head"), Node("thorax"), Node("abdomen")],
-        edges=[
-            Edge(source=Node("head"), destination=Node("thorax")),
-            Edge(source=Node("thorax"), destination=Node("abdomen")),
-        ],
-    )
-    instance1 = Instance(skeleton=skeleton, points={"head": Point(x=0, y=0)})
-    numpy_array1 = np.array([[1, 1], [2, 2], [3, 3]], dtype="float32")
-    numpy_array2 = np.array(
-        [[1, 1, True, False], [2, 2, True, False], [3, 3, True, False]], dtype="float32"
-    )
-    instance2 = instance_from_numpy(points=numpy_array1, skeleton=skeleton)
-    instance3 = instance_from_numpy(points=numpy_array2, skeleton=skeleton)
-
-    # Instances & HDF5
-    assert type(predicted_from_instance(instance1, 0.0)) == PredictedInstance
-    assert type(instance2) == Instance
-    assert type(instance3) == Instance
-    assert type(read_hdf5(slp_file1)) == dict
-
-    # Videos
-    assert type(read_videos(slp_file1)) == list
-    if len(read_videos(slp_file1)) >= 1:
-        assert type(read_videos(slp_file1)[0]) == Video
-
-    # Tracks & Metadata
-    assert type(read_tracks(slp_file1)) == list
-    if len(read_tracks(slp_file1)) >= 1:
-        assert type(read_tracks(slp_file1)[0]) == Track
-    assert type(read_metadata(slp_file1)) == dict
-
-    # Skeleton
-    assert type(read_skeleton(slp_file1)) == list
-    if len(read_skeleton(slp_file1)) >= 1:
-        assert type(read_skeleton(slp_file1)[0]) == Skeleton
-    if len(read_skeleton(slp_file1)) >= 1:
-        assert type(read_skeleton(slp_file1)[0]) == Skeleton
-
-    # Points & PredictedPoints
-    assert type(read_points(slp_file1)) == np.ndarray
-    if len(read_points(slp_file1)) >= 1:
-        assert type(read_points(slp_file1)[0]) == np.void
-    assert type(read_pred_points(slp_file1)) == np.ndarray
-    if len(read_pred_points(slp_file1)) >= 1:
-        assert type(read_pred_points(slp_file1)[0]) == np.void
-
-    # Instances
-    assert type(read_instances(slp_file1)) == list
-    if len(read_instances(slp_file1)) >= 1:
-        assert type(read_instances(slp_file1)[0]) == Instance
-        assert type(read_instances(slp_file1)[2]) == PredictedInstance
-
-    assert read_skeleton(slp_file2)
+def test_read_labels(slp_file1):
+    labels = read_labels(slp_file1)
+    assert type(labels) == Labels
