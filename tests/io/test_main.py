15a64c87a4e4062f607e5ac4e3d71a60a8da9949
Talmo Pereira <talmo@salk.edu>
2022-07-22 22:44:26 -0700

    Fix test

--- tests/io/test_main.py
+++ tests/io/test_main.py
@@ -2,5 +2,5 @@ from sleap_io import Labels
 from sleap_io.io.main import load_slp
 
 
-def test_load_slp(slp_file1):
-    assert type(load_slp(slp_file1)) == Labels
+def test_load_slp(slp_typical):
+    assert type(load_slp(slp_typical)) == Labels
