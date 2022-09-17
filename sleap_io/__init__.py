0a153dcbc813912a3a71aefe13fcc64d11431641
Talmo Pereira <talmo@salk.edu>
2022-09-16 17:58:19 -0700

    Fix version and readme badge (#19)
    * Fix version (whoops)
    
    * Add PyPI badges
    
    * Fix repo on badge
    
    * Add placeholder for python version badge -- needs to be added to package

--- sleap_io/__init__.py
+++ sleap_io/__init__.py
@@ -2,7 +2,7 @@
 
 # Define package version.
 # This is read dynamically by setuptools in setup.cfg to determine the release version.
-__version__ = "0.0.1a0"
+__version__ = "0.0.1"
 
 from sleap_io.model.skeleton import Node, Edge, Skeleton, Symmetry
 from sleap_io.model.video import Video
