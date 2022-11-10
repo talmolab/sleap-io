7524e7b487c1390759683a3e870eb27fdde3f300
Talmo Pereira <talmo@salk.edu>
2022-11-09 22:47:09 -0800

    Refactor high level API

--- sleap_io/__init__.py
+++ sleap_io/__init__.py
@@ -15,5 +15,4 @@ from sleap_io.model.instance import (
 )
 from sleap_io.model.labeled_frame import LabeledFrame
 from sleap_io.model.labels import Labels
-from sleap_io.io.main import load_slp
-from sleap_io.io.nwb import write_labels_to_nwb, append_labels_data_to_nwb
+from sleap_io.io.main import load_slp, save_nwb, load_labelstudio, save_labelstudio
