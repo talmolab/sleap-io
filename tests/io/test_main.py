c7bfec41402f1e827d16da22f5d0b8d4af5a8704
Talmo Pereira <talmo@salk.edu>
2022-12-06 17:52:11 -0800

    Add roundtrip tests

--- tests/io/test_main.py
+++ tests/io/test_main.py
@@ -1,8 +1,30 @@
 """Tests for functions in the sleap_io.io.main file."""
 from sleap_io import Labels
-from sleap_io.io.main import load_slp
+from sleap_io.io.main import (
+    load_slp,
+    load_nwb,
+    save_nwb,
+    load_labelstudio,
+    save_labelstudio,
+)
 
 
 def test_load_slp(slp_typical):
     """Test `load_slp` loads a .slp to a `Labels` object."""
     assert type(load_slp(slp_typical)) == Labels
+
+
+def test_nwb(tmp_path, slp_typical):
+    labels = load_slp(slp_typical)
+    save_nwb(labels, tmp_path / "test_nwb.nwb")
+    loaded_labels = load_nwb(tmp_path / "test_nwb.nwb")
+    assert type(loaded_labels) == Labels
+    assert len(loaded_labels) == len(labels)
+
+
+def test_labelstudio(tmp_path, slp_typical):
+    labels = load_slp(slp_typical)
+    save_labelstudio(labels, tmp_path / "test_labelstudio.json")
+    loaded_labels = load_labelstudio(tmp_path / "test_labelstudio.json")
+    assert type(loaded_labels) == Labels
+    assert len(loaded_labels) == len(labels)
