a7ef4bcb8df251a84afd0b05a2c16614e83686b4
Talmo Pereira <talmo@salk.edu>
2023-06-23 10:33:06 -0700

    Test imports

--- tests/model/test_video.py
+++ tests/model/test_video.py
@@ -1,6 +1,10 @@
 """Tests for methods in the sleap_io.model.video file."""
 from sleap_io import Video
 
+import imageio
+import imageio_ffmpeg
+import av
+
 
 def test_video_class():
     """Test initialization of `Video` object."""
