d4aa283da21081d95d1bdbaf1315cfe8c3e17cec
Talmo Pereira <talmo@salk.edu>
2023-09-12 14:54:55 -0700

    Video handle persistence (#64)
    * Initial MediaVideo handle persistence
    
    * Fix using other backends
    
    * Refactor and add more tests for backend reader type
    
    * Test fixes
    
    * Fix frame counting with imageio backends

--- tests/io/test_video_backends.py
+++ tests/io/test_video_backends.py
@@ -4,6 +4,7 @@ from sleap_io.io.video import VideoBackend, MediaVideo, HDF5Video
 import numpy as np
 from numpy.testing import assert_equal
 import h5py
+import pytest
 
 
 def test_video_backend_from_filename(centered_pair_low_quality_path, slp_minimal_pkg):
@@ -55,35 +56,59 @@ def test_get_frame(centered_pair_low_quality_path):
     assert_equal(backend[-3:-1], backend.get_frames(range(1097, 1099)))
 
 
-def test_mediavideo(centered_pair_low_quality_path):
+@pytest.mark.parametrize("keep_open", [False, True])
+def test_mediavideo(centered_pair_low_quality_path, keep_open):
     backend = VideoBackend.from_filename(
-        centered_pair_low_quality_path, plugin="FFMPEG"
+        centered_pair_low_quality_path, plugin="FFMPEG", keep_open=keep_open
     )
     assert type(backend) == MediaVideo
     assert backend.filename == centered_pair_low_quality_path
     assert backend.shape == (1100, 384, 384, 1)
     assert backend[0].shape == (384, 384, 1)
     assert backend[:3].shape == (3, 384, 384, 1)
-
-    backend = VideoBackend.from_filename(centered_pair_low_quality_path, plugin="pyav")
-    assert type(backend) == MediaVideo
-    assert backend.filename == centered_pair_low_quality_path
-    assert backend.shape == (1100, 384, 384, 1)
-    assert backend[0].shape == (384, 384, 1)
-    assert backend[:3].shape == (3, 384, 384, 1)
+    if keep_open:
+        assert backend._open_reader is not None
+        assert backend[0].shape == (384, 384, 1)
+        assert type(backend._open_reader).__name__ == "LegacyPlugin"
+    else:
+        assert backend._open_reader is None
 
     backend = VideoBackend.from_filename(
-        centered_pair_low_quality_path, plugin="opencv"
+        centered_pair_low_quality_path, plugin="pyav", keep_open=keep_open
     )
     assert type(backend) == MediaVideo
     assert backend.filename == centered_pair_low_quality_path
     assert backend.shape == (1100, 384, 384, 1)
     assert backend[0].shape == (384, 384, 1)
     assert backend[:3].shape == (3, 384, 384, 1)
+    if keep_open:
+        assert backend._open_reader is not None
+        assert backend[0].shape == (384, 384, 1)
+        assert type(backend._open_reader).__name__ == "PyAVPlugin"
+    else:
+        assert backend._open_reader is None
+
+    backend = VideoBackend.from_filename(
+        centered_pair_low_quality_path, plugin="opencv", keep_open=keep_open
+    )
+    assert type(backend) == MediaVideo
+    assert backend.filename == centered_pair_low_quality_path
+    assert backend.shape == (1100, 384, 384, 1)
+    assert backend[0].shape == (384, 384, 1)
+    assert backend[:3].shape == (3, 384, 384, 1)
+    if keep_open:
+        assert backend._open_reader is not None
+        assert backend[0].shape == (384, 384, 1)
+        assert type(backend._open_reader).__name__ == "VideoCapture"
+    else:
+        assert backend._open_reader is None
 
 
-def test_hdf5video_rank4(centered_pair_low_quality_path, tmp_path):
-    backend = VideoBackend.from_filename(centered_pair_low_quality_path)
+@pytest.mark.parametrize("keep_open", [False, True])
+def test_hdf5video_rank4(centered_pair_low_quality_path, tmp_path, keep_open):
+    backend = VideoBackend.from_filename(
+        centered_pair_low_quality_path, keep_open=keep_open
+    )
     imgs = backend[:3]
     assert imgs.shape == (3, 384, 384, 1)
 
@@ -97,6 +122,9 @@ def test_hdf5video_rank4(centered_pair_low_quality_path, tmp_path):
     assert backend[0].shape == (384, 384, 1)
     assert backend[:].shape == (3, 384, 384, 1)
     assert not backend.has_embedded_images
+    if keep_open:
+        assert backend._open_reader is not None
+        assert backend[0].shape == (384, 384, 1)
 
 
 def test_hdf5video_embedded(slp_minimal_pkg):
