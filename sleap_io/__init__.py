f4d2bc1392a3063ec11186c5cb01536a92aa611b
Talmo Pereira <talmo@salk.edu>
2024-04-05 15:00:08 -0700

    Bump version

--- sleap_io/__init__.py
+++ sleap_io/__init__.py
@@ -2,7 +2,7 @@
 
 # Define package version.
 # This is read dynamically by setuptools in pyproject.toml to determine the release version.
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 from sleap_io.model.skeleton import Node, Edge, Skeleton, Symmetry
 from sleap_io.model.video import Video
