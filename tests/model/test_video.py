c6b4e4a4381e6950901e0665a3db5c4033b859b2
Talmo Pereira <talmo@salk.edu>
2024-04-14 00:23:16 -0700

    Video QOL enhancements (#82)
    * Video open/close control
    
    * Replace filename
    
    * Remove outdated test
    
    * Update readme examples
    
    * coverage

--- tests/model/test_video.py
+++ tests/model/test_video.py
@@ -3,6 +3,8 @@
 from sleap_io import Video
 from sleap_io.io.video import MediaVideo
 import numpy as np
+import pytest
+from pathlib import Path
 
 
 def test_video_class():
@@ -18,15 +20,7 @@ def test_video_from_filename(centered_pair_low_quality_path):
     test_video = Video.from_filename(centered_pair_low_quality_path)
     assert test_video.filename == centered_pair_low_quality_path
     assert test_video.shape == (1100, 384, 384, 1)
-
-
-def test_video_auto_backend(centered_pair_low_quality_path):
-    """Test initialization of `Video` object with automatic backend selection."""
-    test_video = Video(filename=centered_pair_low_quality_path)
-    assert test_video.backend is not None
     assert type(test_video.backend) == MediaVideo
-    assert test_video.filename == centered_pair_low_quality_path
-    assert test_video.shape == (1100, 384, 384, 1)
 
 
 def test_video_getitem(centered_pair_low_quality_video):
@@ -40,3 +34,70 @@ def test_video_repr(centered_pair_low_quality_video):
         'Video(filename="tests/data/videos/centered_pair_low_quality.mp4", '
         "shape=(1100, 384, 384, 1), backend=MediaVideo)"
     )
+
+
+def test_video_exists(centered_pair_low_quality_video):
+    video = Video("test.mp4")
+    assert video.exists() is False
+
+    assert centered_pair_low_quality_video.exists() is True
+
+
+def test_video_open_close(centered_pair_low_quality_path):
+    video = Video(centered_pair_low_quality_path)
+    assert video.is_open is False
+    assert video.backend is None
+
+    img = video[0]
+    assert img.shape == (384, 384, 1)
+    assert video.is_open is True
+
+    video = Video("test.mp4")
+    assert video.is_open is False
+    with pytest.raises(FileNotFoundError):
+        video[0]
+
+    video = Video.from_filename(centered_pair_low_quality_path)
+    assert video.is_open is True
+    assert type(video.backend) == MediaVideo
+
+    video.close()
+    assert video.is_open is False
+    assert video.backend is None
+    assert video.shape is None
+
+    video.open()
+    assert video.is_open is True
+    assert type(video.backend) == MediaVideo
+    assert video[0].shape == (384, 384, 1)
+
+    video = Video.from_filename(centered_pair_low_quality_path, grayscale=False)
+    assert video.shape == (1100, 384, 384, 3)
+    video.open()
+    assert video.shape == (1100, 384, 384, 3)
+    video.open(grayscale=True)
+    assert video.shape == (1100, 384, 384, 1)
+
+
+def test_video_replace_filename(centered_pair_low_quality_path):
+    video = Video.from_filename("test.mp4")
+    assert video.exists() is False
+
+    video.replace_filename(centered_pair_low_quality_path)
+    assert video.exists() is True
+    assert video.is_open is True
+    assert type(video.backend) == MediaVideo
+
+    video.replace_filename(Path(centered_pair_low_quality_path))
+    assert video.exists() is True
+    assert video.is_open is True
+    assert type(video.backend) == MediaVideo
+
+    video.replace_filename("test.mp4")
+    assert video.exists() is False
+    assert video.is_open is False
+
+    video.replace_filename(centered_pair_low_quality_path, open=False)
+    assert video.exists() is True
+    assert video.is_open is False
+    assert video.backend is None
