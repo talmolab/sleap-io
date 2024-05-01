61ab2d386805fa3bcf533c59ce2c7bbd8cd2f51b
Talmo Pereira <talmo@salk.edu>
2024-04-30 22:29:34 -0700

    Add `ImageVideo` backend (#88)
    * Add fixtures
    
    * Add backend
    
    * Update high level
    
    * Tests
    
    * Make sure paths are posix
    
    * Exclude py38

--- tests/fixtures/videos.py
+++ tests/fixtures/videos.py
@@ -14,3 +14,13 @@ def centered_pair_low_quality_path():
 def centered_pair_low_quality_video(centered_pair_low_quality_path):
     """Video with two flies in the center."""
     return sleap_io.Video.from_filename(centered_pair_low_quality_path)
+
+
+@pytest.fixture
+def centered_pair_frame_paths():
+    """Paths to three frames of a video with two flies in the center."""
+    return [
+        "tests/data/videos/imgs/img.00.jpg",
+        "tests/data/videos/imgs/img.01.jpg",
+        "tests/data/videos/imgs/img.02.jpg",
+    ]
